029e13c78c07b14010b0bf9278e627bfa89e942b
DreadRemilia <chen980123@126.com>
2020-03-07 21:41:49 +0800

    login/regist module finished

--- app/src/main/java/com/example/appmainframe/Activity/ActivityCollector.java
+++ app/src/main/java/com/example/appmainframe/Activity/ActivityCollector.java
@@ -6,26 +6,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ActivityCollector {
-    //利用集合类存放Activity
-    public static List<Activity> activities = new ArrayList<>();
+  //利用集合类存放Activity
+  public static List<Activity> activities = new ArrayList<>();
 
-    //添加Activity方法
-    public static void addActivity(Activity activity){
-        activities.add(activity);
-    }
+  //添加Activity方法
+  public static void addActivity(Activity activity){
+    activities.add(activity);
+  }
 
-    //移除Activity方法
-    public static void removeActivity(Activity activity){
-        activities.remove(activity);
-    }
+  //移除Activity方法
+  public static void removeActivity(Activity activity){
+    activities.remove(activity);
+  }
 
-    //关闭所有Activity方法
-    public static void finishAll(){
-        for(Activity activity : activities){
-            if(!activity.isFinishing()){
-                activity.finish();
-            }
-        }
-        activities.clear();
+  //关闭所有Activity方法
+  public static void finishAll(){
+    for(Activity activity : activities){
+      if(!activity.isFinishing()){
+        activity.finish();
+      }
     }
+    activities.clear();
+  }
 }
